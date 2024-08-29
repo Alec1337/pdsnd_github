3d875523abbae66ece41438b49c731c16f1a4872
Alec Shore <alec.shore@smartcontract.com>
2024-08-28 20:16:47 -0600

    refactor: Changed Comments)

--- bikeshare.py
+++ bikeshare.py
@@ -130,7 +130,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    """Displays stats on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
@@ -148,7 +148,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays stats on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
